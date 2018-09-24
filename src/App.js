39bbd82fcb9ba0356ce78d97113a4359fdab0885
github username <jeffreyhwang90@gmail.com>
2018-09-24 09:42:25 -0400

    set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Body from './Body';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        HELLO
+        <Header />
+        <Body />
+        <Footer />
       </div>
     );
   }
